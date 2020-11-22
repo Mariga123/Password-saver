2f7ad66093b9c6688dd8d8ebad6decb9e7311e05
Mariga123 <johnmariga8@gmail.com>
2020-11-22 17:33:05 +0300

    define the main function for user interface

--- run.py
+++ run.py
@@ -80,17 +80,17 @@ def display_credentials():
 
 
 def main():
-    print("Hello Welcome to password locker")
+    print("WELCOME TO PASSWORD LOCKER ENTER YOUR NAME")
     user_name = input()
     print(f"Hello {user_name}, sign up to password locker to create an account")
     print('\n')
 
     while True:
-     print("Use these known short codes to operate :\n SU -> SIGN UP.\n DA -> Display your account.\n LN ->LOGIN.\n EX ->exit Pass Word Locker.")   
+     print("Use these known short codes to navigate through :\n SU -> SIGN UP.\n DA -> Display your account.\n LN ->LOGIN.\n EX ->exit Pass Word Locker.")   
      short_code = input().lower()
 
      if short_code == 'su':
-            print("Create a PassWord Locker Account")
+            print("Create your account")
             print("_"*100)
             account_name = input('Account name:')
             print ('\n')
@@ -105,10 +105,71 @@ def main():
             print(f"You can now login to your {account_name} account using your password.")
             print ('\n')
      elif short_code == "ex":
-            print(f"Thanks {user_name} for your time.I hope you enjoyed my service.Bye...")
+            print(f"Thanks {user_name} for trying our password locker")
             break
+            
 
+     elif short_code == 'da':
+             if display_accounts():
+                 print("Displaying your account and details")
+                 print('\n')
+                 for account in display_accounts():
+                     print(f"Account name:{account.account_name}  User name: {account.user_name} Password:{account.password}")
+                     print('\n')
+             else:
+                 print('\n')
+                 print("You need to create an account first")
+                 print('\n')
 
+     elif short_code == 'ln':
+            print("Enter your password to login.")
+            search_account = input()
+            if check_existing_accounts(search_account):
+                print("\033[1;32;1m   \n")
+                print(f"Your account {account_name} has been logged out succesfully")
+                print("\033[1;37;1m   \n")  
+
+                while True:
+                    print('''Use these short codes:
+                    CA -> Create new credential.
+                    DC -> Display your credentials list
+                    ex ->Log out your credentials account. ''')
+                    short_code = input().lower()
+                    if short_code == "ca":
+                        print("Create new credential")
+                        print('_' * 20)
+                        credentials_name = input('Credential name:')
+                        print('\n')
+                        usr_name = input(f"{credentials_name} user name:")
+                        print('\n')
+                        print('*' * 20)
+                        pwd = input(f"{credentials_name} password:")
+                        save_credentials(create_credentials(credentials_name,u_name,pwd,e_address))
+                        print('\n')
+                        print(f"A New {credentials_name} Account with the user name  {usr_name} has been created.")
+                        print ('\n')  
+
+                    elif short_code == 'dc':
+                         if display_credentials():
+                             print("Here is your credentials")
+                             print('\n')
+                             for credentials in display_credentials():
+                                 print(f"Credential name:{credentials.credentials_name}  User name: {credentials.name} Password:{credentials.password}")
+                                 print('\n')
+                         else:
+                              print('\n')
+                              print("create an account first")
+                              print('\n')  
+
+                    elif short_code == "ex":
+                        print('\n')
+                        print(f"Your account {account_name} has been logged out successfully")
+                        print('\n')
+                        break
+         
+     else:
+        print("confirm your selection and try again") 
+        print('\n')    
 
 
 if __name__ == "__main__":
