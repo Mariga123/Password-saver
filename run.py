7b2f19942cc8de6de0182d7f326f8363994c9d86
Mariga123 <johnmariga8@gmail.com>
2020-11-22 17:23:36 +0300

    user interface for creating ac

--- run.py
+++ run.py
@@ -79,6 +79,37 @@ def display_credentials():
     return Credentials.display_credentials() 
 
 
+def main():
+    print("Hello Welcome to password locker")
+    user_name = input()
+    print(f"Hello {user_name}, sign up to password locker to create an account")
+    print('\n')
+
+    while True:
+     print("Use these known short codes to operate :\n SU -> SIGN UP.\n DA -> Display your account.\n LN ->LOGIN.\n EX ->exit Pass Word Locker.")   
+     short_code = input().lower()
+
+     if short_code == 'su':
+            print("Create a PassWord Locker Account")
+            print("_"*100)
+            account_name = input('Account name:')
+            print ('\n')
+            u_name = input('User name:')
+            print ('\n')
+            pwd = input('Password : ')
+            print ('\n')
+            e_address = input('Email address:')
+            save_accounts(create_account(account_name,u_name,pwd,e_address)) 
+            print ('\n')
+            print(f"A New {account_name} Account with the user name  {u_name} has been created.")
+            print(f"You can now login to your {account_name} account using your password.")
+            print ('\n')
+     elif short_code == "ex":
+            print(f"Thanks {user_name} for your time.I hope you enjoyed my service.Bye...")
+            break
+
+
+
 
 if __name__ == "__main__":
     main()             
